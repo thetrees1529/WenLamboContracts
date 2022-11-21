fc8e81b0a13dcd10baa88acb3ff8e18fe8034f30
Luke Hannigan <lukeyhannigany@gmail.com>
2022-11-21 18:51:49 +0000

    earn

--- contracts/Earn.sol
+++ contracts/Earn.sol
@@ -1,201 +1,208 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@thetrees1529/solutils/contracts/gamefi/OwnerOf.sol";
 import "@thetrees1529/solutils/contracts/payments/Fees.sol";
 import "@thetrees1529/solutils/contracts/payments/ERC20Payments.sol";
-import "./AHILLE.sol";
+import "./Token.sol";
 
-contract Earn is Ownable, OwnerOf, ERC20Payments {
+contract Earn is AccessControl {
 
     using Fees for uint;
+    using ERC20Payments for IERC20;
 
-    struct Stage {
+    struct Payment {
+        IERC20 token;
+        uint value;
+    }
+
+    struct Substage {
         string name;
-        uint price;
-        uint priceHville;
+        Payment[] payments;
         uint emission;
     }
 
-    struct Nfv {
-        bool onLadder;
-        bool claimedBefore;
-        uint stage;
-
-        uint lastClaimed; //timestamp
-        uint firstClaimedAt;
-        uint totalClaimed;
-
-        uint lockedTotal;
-        uint lockedClaimed;
-
-        bool claimedInterestBefore;
-        uint lastClaimedInterest;
-        uint totalInterestClaimed;
+    struct Stage {
+        string name;
+        Substage[] substages;
     }
 
-    Stage[] private _ladder;
-    uint public defaultEmission;
-    Fees.Fee public lockRatio;
-    Fees.Fee public interest;
+    struct Location {
+        uint stage;
+        uint substage;
+    }
+
+    struct Nfv {
+        bool onStages;
+        bool claimedOnce;
+        uint lastClaim;
+        uint pendingClaim;
+        uint locked;
+        uint unlocked;
+        Location location;
+    }
+
+    bytes32 public EARN_ROLE = keccak256("EARN_ROLE"); 
+
+    uint public genesis;
     uint public unlockStart;
     uint public unlockEnd;
-    AHILLE public ahille;
-    IERC20 public hville;
-    uint public deployedAt;
-    uint private _totalYield;
-    uint public globalMaxYield;
+    uint public baseEarn;
+    uint public mintCap;
+    Stage[] private _stages;
+    Fees.Fee public lockRatio;
+    Fees.Fee public interest;
+    Token public token;
+    IERC721 public nfvs;
+    ERC20Payments.Payee[] private _payees;
 
-    constructor(IERC721 nfvs, IERC20 hville_, address ahille_, ERC20Payments.Payee[] memory payees, Stage[] memory ladder_, uint defaultEmission_, Fees.Fee memory lockRatio_, Fees.Fee memory interest_, uint unlockStart_, uint unlockEnd_, uint globalMaxYield_) OwnerOf(nfvs) ERC20Payments(IERC20(ahille_)) {
-        _setPayees(payees);
-        ahille = AHILLE(ahille_);
-        hville = hville_;
+    mapping(uint => Nfv) public nfvInfo;
+
+    constructor(IERC721 nfvs_, Token token_, Stage[] memory stages, Fees.Fee memory lockRatio_, Fees.Fee memory interest_, uint unlockStart_, uint unlockEnd_, uint baseEarn_, uint mintCap_) {
+        token = token_;
+        nfvs = nfvs_;
+        _stages = stages;
         lockRatio = lockRatio_;
         interest = interest_;
-        for(uint i; i < ladder_.length; i++) {
-            _ladder.push(ladder_[i]);
-        }
-        defaultEmission = defaultEmission_;
+        genesis = block.timestamp;
         unlockStart = unlockStart_;
         unlockEnd = unlockEnd_;
-        deployedAt = block.timestamp;
-        globalMaxYield = globalMaxYield_;
+        baseEarn = baseEarn_;
+        mintCap = mintCap_;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function setPayees(ERC20Payments.Payee[] memory payees) external onlyOwner {
-        _setPayees(payees);
+    function setPayees(ERC20Payments.Payee[] calldata payees) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _payees = payees;
     }
 
-    function ladder() external view returns(Stage[] memory) {return _ladder;}
+    function getPayees() external view returns(ERC20Payments.Payee[] memory) {return _payees;}
 
-    function getNfv(uint tokenId) external view returns(Nfv memory) {
-        return _nfvs[tokenId];
+    function getStages() external view returns(Stage[] memory) {
+        return _stages;
     }
 
-    function getLocked(uint tokenId) public view returns(uint) {
-        Nfv storage nfv = _nfvs[tokenId]; 
-        return nfv.lockedTotal - nfv.lockedClaimed;
+    function getClaimable(uint tokenId) external view returns(uint) {
+        Nfv storage nfv = nfvInfo[tokenId];
+        return nfv.pendingClaim + _getPending(tokenId);
     }
 
-    function getUnlockable(uint tokenId) public view returns(uint) {
-        if(_isBeforeUnlock()) return 0;
-        Nfv storage nfv = _nfvs[tokenId]; 
-        return ((nfv.lockedTotal * (block.timestamp - unlockStart)) / (unlockEnd - unlockStart)) - nfv.lockedClaimed;
+    function getLocked(uint tokenId) external view returns(uint){
+        Nfv storage nfv = nfvInfo[tokenId];
+        uint totalTime = unlockEnd - unlockStart;
+        uint timeElapsed; 
+        if(block.timestamp >= unlockStart) timeElapsed = block.timestamp - unlockStart;
+        uint timeUnlocking = timeElapsed <= totalTime ? timeElapsed : totalTime;
+        uint theoreticalLocked = (nfv.locked * timeUnlocking) / totalTime;
+        return theoreticalLocked - nfv.unlocked;
     }
 
-    function getClaimable(uint tokenId) public view returns(uint) {
-        Nfv storage nfv = _nfvs[tokenId];
-        uint earningSince = _earningSinceOf(nfv);
-        uint emission = nfv.onLadder ? _ladder[nfv.stage].emission : defaultEmission;
-        uint attemptedClaim = _calcEarnedSince(earningSince, emission);
-        return _getMaxClaim(attemptedClaim);
-    }
 
-    function getInterestOf(uint tokenId) public view returns(uint) {
-        Nfv storage nfv = _nfvs[tokenId];
-        if(!nfv.claimedBefore) return 0;
-        uint locked = getLocked(tokenId);
-        uint emission = locked.feesOf(interest);
-        uint attemptedClaim = _calcEarnedSince(nfv.claimedInterestBefore ? nfv.lastClaimedInterest : nfv.firstClaimedAt, emission);
-        return _getMaxClaim(attemptedClaim);
-    }
 
-    function upgradeMultiple(uint[] calldata tokenIds) public {
-        for(uint i; i < tokenIds.length; i ++) upgrade(tokenIds[i]);
+    function claim(uint tokenId) public onlyOwnerOf(tokenId) {
+        _claim(tokenId);
+        Nfv storage nfv = nfvInfo[tokenId];
+        uint locked = nfv.pendingClaim.feesOf(lockRatio);
+        uint toOwner = nfv.pendingClaim - locked;
+        delete nfv.pendingClaim;
+        nfv.locked += locked;
+        token.mintTo(msg.sender, toOwner);
     }
 
     function upgrade(uint tokenId) public onlyOwnerOf(tokenId) {
-        claim(tokenId);
-
-        Nfv storage nfv = _nfvs[tokenId]; 
-        
-        uint stageIndex;
-
-        if(nfv.onLadder) {
-            nfv.stage ++;
-            stageIndex = nfv.stage;
-        } else nfv.onLadder = true;
-
-        Stage storage stage = _ladder[stageIndex];
-        
-        ahille.transferFrom(msg.sender, address(this), stage.price);
-        hville.transferFrom(msg.sender, address(this), stage.priceHville);
-        _makePayment(stage.price);
+        Nfv storage nfv = nfvInfo[tokenId];
+        Location memory location = nfv.location;
+        if(nfv.onStages) {
+            Stage storage currentStage = _stages[location.stage];
+            if(location.substage >= currentStage.substages.length - 1) {
+                location.stage ++;
+                location.substage = 0;
+            } else {
+                location.substage ++;
+            }
+        }
+        _setLocation(tokenId, location);
+        Substage storage substage = _getSubstage(location);
+        for(uint i; i < substage.payments.length; i ++) {
+            _takePayment(msg.sender, substage.payments[i]);
+        }
     }
 
-    function claimMultiple(uint[] calldata tokenIds) public {
-        for(uint i; i < tokenIds.length; i ++) claim(tokenIds[i]);
+    function isInLocation(uint tokenId) external view returns(bool) {
+        return nfvInfo[tokenId].onStages;
     }
 
-    function claim(uint tokenId) public onlyOwnerOf(tokenId) {
-        claimInterest(tokenId);
-        Nfv storage nfv = _nfvs[tokenId];
-        uint claimable = getClaimable(tokenId);
-        uint locked = claimable.feesOf(lockRatio);
-        uint toOwner = claimable - locked;
-        nfv.lockedTotal += locked;
-        nfv.lastClaimed = block.timestamp;
-        nfv.totalClaimed += claimable;
-        if(!nfv.claimedBefore) {nfv.claimedBefore = true; nfv.firstClaimedAt = block.timestamp;}
-        _totalYield += claimable;
-        ahille.mintTo(msg.sender, toOwner);
-    }
-
-    function claimInterestMultiple(uint[] calldata tokenIds) public {
-        for(uint i; i < tokenIds.length; i ++) claimInterest(tokenIds[i]);
-    }
-
-    function claimInterest(uint tokenId) public onlyOwnerOf(tokenId) {
-        Nfv storage nfv = _nfvs[tokenId];
-        uint claimable = getInterestOf(tokenId);
-        nfv.lastClaimedInterest = block.timestamp;
-        nfv.totalInterestClaimed += claimable;
-        if(!nfv.claimedInterestBefore) nfv.claimedInterestBefore = true;
-        _totalYield += claimable;
-        ahille.mintTo(msg.sender, claimable);
-    }
-
-    function claimLockedMultiple(uint[] calldata tokenIds) public {
-        for(uint i; i < tokenIds.length; i ++) claimLocked(tokenIds[i]);
-    }
-
-    function claimLocked(uint tokenId) public onlyOwnerOf(tokenId) {
-        claimInterest(tokenId);
-        Nfv storage nfv = _nfvs[tokenId];
-        uint claimable = getUnlockable(tokenId);
-        nfv.lockedClaimed += claimable;
-        ahille.mintTo(msg.sender, claimable);
-    }
-
-    function _getMaxClaim(uint attemptedClaim) private view returns(uint) {
-        uint maxClaim = globalMaxYield - _totalYield;
-        return attemptedClaim <= maxClaim ? attemptedClaim : maxClaim;
-    }
-
-    function _earningSinceOf(Nfv storage nfv) private view returns(uint) {
-        return nfv.claimedBefore ? nfv.lastClaimed : deployedAt;
-    }
-
-    function _calcEarnedDuring(uint start, uint end, uint emission) private pure returns(uint) {
-        return (end - start) * emission;
-    }
-
-    function _calcEarnedSince(uint timestamp, uint emission) private view returns(uint) {
-        return _calcEarnedDuring(timestamp, block.timestamp, emission);
+    function getLocation(uint tokenId) external view returns(Location memory) {
+        Nfv storage nfv = nfvInfo[tokenId];
+        require(nfv.onStages, "Not in a location.");
+        return nfv.location;
     } 
 
-
-    function withdrawHville() external onlyOwner {
-        hville.transfer(msg.sender, hville.balanceOf(address(this)));
+    function setLocation(uint tokenId, Location calldata location) external onlyRole(EARN_ROLE) {
+        _setLocation(tokenId, location);
     }
 
-    function _isBeforeUnlock() private view returns(bool) {
-        return block.timestamp < unlockStart;
+    function _setLocation(uint tokenId, Location memory location) private {
+        require(_isValidLocation(location), "Setting invalid location.");
+        _claim(tokenId);
+        Nfv storage nfv = nfvInfo[tokenId];
+        if(!nfv.onStages) nfv.onStages = true;
+        nfv.location = location;
     }
 
-    mapping(uint => Nfv) private _nfvs;
-    
+    function exitLocation(uint tokenId) external onlyRole(EARN_ROLE) {
+        _claim(tokenId);
+        Nfv storage nfv = nfvInfo[tokenId];
+        if(nfv.onStages) {
+            nfv.onStages = false;
+            delete nfv.location;
+        }
+    }
+
+    function _claim(uint tokenId) private {
+        uint claimed = _getPending(tokenId);
+        Nfv storage nfv = nfvInfo[tokenId];
+        nfv.lastClaim = block.timestamp;
+        nfv.pendingClaim += claimed;
+        if(!nfv.claimedOnce) nfv.claimedOnce = true;
+    }
+
+    function _getPending(uint tokenId) private view returns(uint) {
+        Nfv storage nfv = nfvInfo[tokenId];
+        uint earningSince = nfv.claimedOnce ? nfv.lastClaim : genesis;
+        Location storage location = nfv.location;
+        uint emission = nfv.onStages ? _getSubstage(location).emission : baseEarn;
+        uint timeEarning = block.timestamp - earningSince;
+        return timeEarning * emission;
+    }
+
+    function _getSubstage(Location memory location) private view returns(Substage storage) {
+        require(_isValidLocation(location), "Location is invalid.");
+        return _stages[location.stage].substages[location.substage];
+    }
+
+    function _isValidLocation(Location memory location) private view returns(bool) {
+        return location.stage < _stages.length && location.substage < _stages[location.stage].substages.length;
+    }
+
+
+    function _takePayment(address from, Payment storage payment) private {
+        payment.token.transferFrom(from, address(this), payment.value);
+        payment.token.split(payment.value, _payees);
+    }
+
+    //never needs to be used unless there is a bug.
+    function withdraw(uint value) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        token.transfer(msg.sender, value);
+    }
+
+    modifier onlyOwnerOf(uint tokenId) {
+        require(OwnerOf.isOwnerOf(nfvs, msg.sender, tokenId), "Does not own NFT.");
+        _;
+    }
+
+
 }
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract AHILLE is ERC20, AccessControl {
+contract Token is ERC20, AccessControl {
 
     struct TransferInput {
         address to;
