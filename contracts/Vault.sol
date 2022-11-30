2748cb3fed5c21628702d07ca87eb788f7e3e063
Luke Hannigan <lukeyhannigany@gmail.com>
2022-11-30 16:22:03 +0000

    vault

---
+++ contracts/Vault.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Vault is AccessControl {
+    bytes32 public VAULT_ROLE = keccak256("VAULT_ROLE");
+    constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+    function withdraw(IERC20 token, address to, uint amount) external onlyRole(VAULT_ROLE) {
+        token.transfer(to, amount);
+    }
+}
---
+++ scripts/deployEarn.js
@@ -0,0 +1,35 @@
+const { ethers } = hre
+
+const {
+nft,
+ tokenAddr ,
+ lockDelay ,
+ lockPeriod , 
+ lockRatio , 
+ interest , 
+ baseEarn , 
+ mintCap ,
+ stages: config
+} = require("./earnConfigs.js")["fujiLambo"]
+
+async function main() {
+    const token = await ethers.getContractAt("Token", tokenAddr)
+    const Contract = await ethers.getContractFactory("Earn")
+    const unlockStart = Date.now() + lockDelay
+    const contract = await (await Contract.deploy(
+        nft,
+        tokenAddr,
+        config,
+        lockRatio,
+        interest,
+        unlockStart,
+        unlockStart + lockPeriod,
+        baseEarn,
+        mintCap
+    )).deployed()
+    await (await token.grantRole(await token.TOKEN_MAESTRO_ROLE(), contract.address)).wait()
+    console.log(contract.address)
+}
+
+main()
+
