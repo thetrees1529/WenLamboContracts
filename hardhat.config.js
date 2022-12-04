ecb5c6666ecfc7bd04ff316bd30d381e1df9f578
Luke Hannigan <lukeyhannigany@gmail.com>
2022-12-04 17:38:01 +0000

    check if privatekey exists

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+const fs = require("fs")
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks: {
+}
+if(fs.existsSync("./privateKey.json")) {
+  module.exports.networks = {
     fuji: {
       url: "https://rpc.ankr.com/avalanche_fuji",
       accounts: [require("./privateKey.json")]
     }
   }
-};
+}
