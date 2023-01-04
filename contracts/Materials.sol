530d7bcda22ea7c7e8b13fbcedf478dfe8c35122
unknown <lukeyhannigany@gmail.com>
2023-01-04 19:15:17 +0000

    materials

---
+++ contracts/Materials.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC1155/presets/ERC1155PresetMinterPauser.sol";
+
+contract Items is ERC1155PresetMinterPauser {
+
+    constructor(string memory URI) ERC1155PresetMinterPauser(URI) {}
+
+    function setURI(string memory URI) external onlyRole(DEFAULT_ADMIN_ROLE) {_setURI(URI);}
+
+}
+
