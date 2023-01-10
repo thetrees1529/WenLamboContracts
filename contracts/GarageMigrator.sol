13f73c0be0bd18a8d2c49531eee48caeaecd2ab1
thetrees1529 <54856258+thetrees1529@users.noreply.github.com>
2023-01-10 21:32:35 +0000

    Update GarageMigrator.sol

--- contracts/GarageMigrator.sol
+++ contracts/GarageMigrator.sol
@@ -21,7 +21,7 @@ contract GarageMigrator is AccessControl {
     function migrate(uint numberOf) external onlyRole(MIGRATOR_ROLE) {
         uint end = checkPoint + numberOf;
         for(uint i = checkPoint; i < end; i ++) {
-            uint tokenId = i + checkPoint;
+            uint tokenId = i;
 
             Earn.Location memory location = source.getLocation(tokenId);
             uint claimable = source.getClaimable(tokenId);
