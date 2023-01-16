7aad1af456aac738b38aa7989466437c7a89cec5
Luke Hannigan <lukeyhannigany@gmail.com>
2023-01-16 17:26:18 +0000

    locked boost

---
+++ contracts/LockedBoost.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./Earn.sol";
+
+contract LockedBoost is AccessControl {
+
+    bytes32 public SLASH_ROLE = keccak256("SLASH_ROLE");
+
+    uint public checkPoint = 0;
+
+    Earn public target;
+
+    uint public numerator;
+    uint public denominator;
+    constructor(Earn target_, uint numerator_, uint denominator_) {
+        numerator = numerator_;
+        denominator = denominator_;
+        target = target_;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(SLASH_ROLE, msg.sender);
+    }
+    function slash(uint numberOf) external onlyRole(SLASH_ROLE) {
+        uint end = checkPoint + numberOf;
+        for(uint i = checkPoint; i < end; i ++) {
+            uint tokenId = i;
+            (bool onStages,,,uint pendingClaim,,,,,,Earn.Location memory location) = target.nfvInfo(tokenId);
+            if(onStages && location.stage == 3) {
+                uint toAdd = (pendingClaim * denominator) / numerator;
+                target.addToLocked(tokenId, toAdd);    
+            }
+
+        }
+        checkPoint += numberOf;
+    }
+}
