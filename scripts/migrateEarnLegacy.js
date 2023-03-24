b3b3770ee2057ebfb1b5e63f6fcc9eb81620c196
Luke Hannigan <lukeyhannigany@gmail.com>
2023-03-24 02:43:36 +0000

    migrate earn script

---
+++ scripts/migrateEarnLegacy.js
@@ -0,0 +1,28 @@
+const { ethers } = hre
+
+const contractAddress = "0xE94b2CB054D88e1763c1D01DB66005d0de37782d"
+const start = 0
+const end = 25
+const interval = 25
+
+let migrator
+
+async function main() {
+    migrator = await ethers.getContractAt("EarnMigratorForLegacy", contractAddress)
+
+    for(let i = start; i < end - start; i += interval) {
+
+        const tokenIds = Array.from(Array(interval).keys()).map(el => el + i)
+        await migrate(tokenIds)
+
+    }
+}
+
+async function migrate(tokenIds) {
+    console.log("Doing: ",tokenIds)
+
+    await (await migrator.migrateList(tokenIds)).wait()
+
+}
+
+main()
