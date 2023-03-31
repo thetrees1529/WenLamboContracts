2810b418ba1695520c18fc5d5ea9eff317d60762
thetrees1529 <54856258+thetrees1529@users.noreply.github.com>
2023-03-31 19:23:05 +0100

    mods script

--- scripts/deployMods.js
+++ scripts/deployMods.js
@@ -1,12 +1,5 @@
 const randomAddress = "0x0305AcBe6a99ABe9ba00647A730104CB12B95032"
-//toolboxes
-const toolboxesUri = "bruh"
-const tokenAddress = "0x5138f9fDAFdDb313Fff6FdDbAf86FB61734C1ce9"
-const payees = [["0xABCD0baDa7ad8d922DD687Cc61FFc65c75C2F8FD",1]]
-const price = "100000000000000000000"
-const config = [[0,1],[1,1],[2,1]]
-
-//mods
+const toolboxesAddress = "0x3Bea526Ae0fB5b68D51702Bb67a03836D599191B"
 const name = "Mods"
 const symbol = "MODS"
 const modsURI = "modsUri"
@@ -15,19 +8,13 @@ const options = [[0,1],[1,1],[2,1],[3,1]]
 const perInputs = [[0,1],[1,2],[2,3]]
 const attributeConfigs = [["tires",100],["power",100], ["handing",100],["speed",100]]
 
-//deploy and set roles
 async function main() {
-    const token = await ethers.getContractAt("Token", tokenAddress)
+    const toolboxes = await ethers.getContractAt("Toolboxes", toolboxesAddress)
     const nfvs = await ethers.getContractAt("Nfvs", nfvsAddress)
     const random = await ethers.getContractAt("Random", randomAddress)
-    const Toolboxes = await ethers.getContractFactory("Toolboxes");
-    const toolboxes =  await (await Toolboxes.deploy(toolboxesUri, random.address, token.address, payees, price, config)).deployed();
     const Mods = await ethers.getContractFactory("Mods");
     const mods =  await (await Mods.deploy(toolboxes.address, random.address, name,symbol,modsURI,options,perInputs,attributeConfigs, nfvs.address)).deployed();
-    await (await token.grantRole(await token.TOKEN_MAESTRO_ROLE(), toolboxes.address)).wait()
-    await (await random.grantRole(await random.CONSUMER_ROLE(), toolboxes.address)).wait()
     await (await random.grantRole(await random.CONSUMER_ROLE(), mods.address)).wait()
-    console.log("Toolboxes deployed to:", toolboxes.address);
     console.log("Mods deployed to:", mods.address);
 }
 
