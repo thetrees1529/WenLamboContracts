a1307ef03bab5e7e6d8c2078908f57f1610b3574
thetrees1529 <54856258+thetrees1529@users.noreply.github.com>
2023-12-21 21:38:05 +0000

    earn helper

--- contracts/Rewards/UnlockHelper.sol
+++ contracts/Rewards/UnlockHelper.sol
@@ -8,7 +8,11 @@ contract UnlockHelper {
 
     function unlockAll(Earn earn) external {
         Nfvs nfvs = earn.nfvs();
-        uint[] memory tokens = nfvs.tokensOfOwner(msg.sender);
+        unlockList(earn, nfvs.tokensOfOwner(msg.sender));
+    }
+
+    function unlockList(Earn earn, uint[] memory tokens) public {
+        Nfvs nfvs = earn.nfvs();
         for (uint i = 0; i < tokens.length; i++) {
             nfvs.safeTransferFrom(msg.sender, address(this), tokens[i]);
             earn.unlock(tokens[i]);
